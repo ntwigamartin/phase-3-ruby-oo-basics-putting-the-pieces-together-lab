3325965f20edbc10f1d96d7d0dad0adc7a4461c8
ntwigamartin <ntwiga.martin@gmail.com>
2023-02-10 00:57:04 +0300

    Completed, all tests passing

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+    attr_writer :turn_page
+    
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+# book = Book.new(:title)
+# pp book.title
+# pp book.author
+# pp book.page_count
+# pp book.genre
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+    attr_writer :cobble
+    
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+# shoe = Shoe.new(:brand)
+# shoe.condition = "new"
